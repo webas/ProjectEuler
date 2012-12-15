ba039594bcc096bf744eb0e4da4bc9c656c2dae0
webas <github@knosta.com>
2012-12-15 01:10:29 -0800

    added

---
+++ src/problems/euler29.java
@@ -0,0 +1,24 @@
+package problems;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class euler29 {
+
+	
+	public static void main(String[] args) { 
+		List M = new ArrayList();
+		
+		for(int a = 2; a <= 100; a++) {
+			for(int b = 2; b <= 100; b++) {
+				if(!M.contains(Math.pow(a, b))) {
+					M.add( Math.pow(a,b));
+				}
+			}
+		}	
+		System.out.println(M.size());
+	}
+
+}
